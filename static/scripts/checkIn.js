5040bf08a212b369ab12acc839abdbe3a14cb20a
Tom Sharp <tomsharp924@gmail.com>
2025-11-15 16:43:49 -0800

    Added form id to all id's to avoid collision

--- static/scripts/checkIn.js
+++ static/scripts/checkIn.js
@@ -1,7 +1,8 @@
 const form_id = document.getElementById("form_id").textContent;
-const loading = document.getElementById("loading");
-const content = document.getElementById("content");
-const error = document.getElementById("error");
+const loading = document.getElementById(`loading-${form_id}`);
+const content = document.getElementById(`content-${form_id}`);
+const error = document.getElementById(`error-${form_id}`);
+const form = document.getElementById(`form-holder-${form_id}`)
 
 document.addEventListener("DOMContentLoaded", () => {
     fetch(`/get-form/${form_id}`)
@@ -9,14 +10,14 @@ document.addEventListener("DOMContentLoaded", () => {
         .then(data => { 
             console.log(data);
             if (data.status_code != 200) {
-                const response_span = document.getElementById("response-span");
+                const response_span = document.getElementById(`response-span-${form_id}`);
                 response_span.textContent = data.status_code;
                 const className = data.status_code >= 200 && data.status_code < 300? "status-ok" :
                                   data.status_code >= 300 && data.status_code < 400? "status-redirect" :
                                   data.status_code >= 400 && data.status_code < 500? "status-client-error" :
                                   data.status_code >= 500 && data.status_code < 600? "status-server-error" : "status-unknown"
                 response_span.classList.add(className)
-                const message = document.getElementById("message-p");
+                const message = document.getElementById(`message-p-${form_id}`);
                 message.textContent = `Message: ${data.message}`
                 setTimeout(() => {
                     error.style.display = "grid"
